86d9a3adc68f0c072fdec016432be56d4e5eeafd
Bjoern <onoofelogmail.com>
2026-03-31 12:10:09 +0200

    - adjusted function to replace character image according to instance

--- script.js
+++ script.js
@@ -32,7 +32,7 @@ function cardTemplate(pokemon){
             <h2>${pokemon.name}</h2>
         </section>
         <section class="pokemon-canvas">
-            <img class="pokemon-img" src="./assets/img/132.webp" alt="">
+            <img class="pokemon-img" src="${pokemon.sprites.front_default}" alt="">
         </section>
         <section class="pokemon-type">
             <img class="iconType" id="${pokemon.name}-type-1" src="">
